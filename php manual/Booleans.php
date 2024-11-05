f219ab57f0ca2c804095c42930d48bcdd54ba6c3
foysal41 <mdfoysal.mf33@gmail.com>
2024-11-06 01:07:57 +0600

    what is booleans

---
+++ php manual/Booleans.php
@@ -0,0 +1,19 @@
+<?php 
+//$boo = true/false;
+
+/*
+$action = true;
+
+if($action == true){
+    echo "version 1.0.0";
+
+}else{
+    echo "Nothing Show here";
+}
+*/
+
+
+var_dump((bool)  -0);
+
+
+?>
